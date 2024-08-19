388b0eded0ed60f03b648358f3d25d7882f5a794
yan <yan.altar123@gmail.com>
2024-08-19 21:53:21 +0800

    updated

--- components/Benefits.tsx
+++ components/Benefits.tsx
@@ -8,18 +8,21 @@ export function Benefits() {
         <h2 className="heading mx-auto text-xl md:text-5xl font-bold mb-4 mt-28 pt-16 md:mb-24 text-center">
           <span className="text-cyan-400">Benefits</span> of applying.
         </h2>
-
-        <div className="dark:bg-zinc-900 flex items-center justify-between gap-4 p-4 md:p-6">
-          <div className="flex-1 text-center md:text-left">Company X</div>
+        <div className="flex-1 text-xl font-extrabold text-start md:text-left">
+          Company X
+        </div>
+        <div className="dark:bg-zinc-900 flex items-center justify-center gap-4 p-4 md:p-6">
           <Compare
             firstImage="/companyx.png"
             secondImage="/newphase.png"
             firstImageClassName="object-cover object-left-top"
             secondImageClassname="object-cover object-left-top"
-            className="h-[200px] w-[200px] md:h-[700px] md:w-[700px]"
+            className="h-[300px] w-[300px] md:h-[700px] md:w-[700px]"
             slideMode="hover"
           />
-          <div className="flex-1 text-center md:text-right">New Phase</div>
+        </div>
+        <div className="flex-1 text-xl font-extrabold text-end md:text-right">
+          New Phase
         </div>
       </section>
     </>
